6a8ca79cf0c3c91aee7489991dd7aaf42bf39dd2
Nikita328 <nikitasurygin913gmail.com>
2021-04-10 09:49:59 +0300

    1234r5

---
+++ 7547547474.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double a2 = Math.Pow(a, 2);
+            Console.WriteLine("Ответ:" + a2);
+            Console.ReadKey();
+     } } }
